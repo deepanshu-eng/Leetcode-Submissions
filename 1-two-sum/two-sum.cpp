8aa32c6eef1a1f42f2eaea542f82df350ae4b175
Princess-Bubblegum-TheSecond <deepanshuthakurdd@gmail.com>
2026-07-14 08:04:38 +0530

    Time: 45 ms (14.91%) | Memory: 14 MB (94.92%) - LeetSync

---
+++ 1-two-sum/two-sum.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    
+    vector<int> twoSum(vector<int>& nums, int target)
+    {
+        std::vector<int> ans{2, 0};
+        for(int i = 0; i <= nums.size() - 1; i++)
+        {
+            for(int j = i + 1; j <= nums.size() - 1; j++)
+            {
+                if(nums[i] + nums[j] == target)
+                {
+                    ans[0] = i;
+                    ans[1] = j;
+                    return ans;
+                }
+            }
+        }  
+        return ans;
+    }
+};
